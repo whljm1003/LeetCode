fcf7b6c4696c3eefba287284c0851e7af394913c
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2022-05-23 14:28:08 +0900

    Time: 133 ms (37.85%), Space: 42.1 MB (88.90%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,11 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+  let answer = [];
   for(let i=0 ; i<nums.length ; i++) {
     for(let j=i+1 ; j<nums.length ; j++) {
       if(nums[i] + nums[j] === target) {
-        return [i, j]
+        answer.push(i, j);
+        break;
       }
     }
   }  
+  return answer;
 };
